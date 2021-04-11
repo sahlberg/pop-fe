4225c7d0212a0a65301fc1966c74ccf081c1e4c4
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2021-04-11 16:27:46 +1000

    initial commit. Pop-fe, popstation(in python) and bchunk(in python)
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

---
+++ bchunk.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+import argparse
+import os
+import re
+import struct
+import sys
+
+
+class bchunk(object):
+    SECTLEN         = 2352
+    WAV_FORMAT_HLEN = 24
+    WAV_DATA_HLEN   = 8
+    
+    def get_file_name(self, line):
+        # strip off leading 'FILE '
+        pos = line.lower().index('file ')
+        line = line[pos + 5:]
+        # strip off leading 'FILE '
+        pos = line.lower().index(' binary')
+        line = line[:pos+1]
+        #strip off leading ' '
+        while line[0] == ' ':
+            line = line[1:]
+        #strip off trailing ' '
+        while line[-1] == ' ':
+            line = line[:-1]
+        # remove double quotes
+        if line[0] == '"':
+            line = line[1:-1]
+        # remove single quotes
+        if line[0] == '\'':
+            line = line[1:-1]
+        return line
+
+    def get_mode(self, track, line):
+        if line.lower().find('mode1/2352') >= 0:
+            track['extension'] = 'iso'
+            track['bstart'] = 16
+            track['bsize'] = 2048
+            return track
+        if line.lower().find('mode2/2352') >= 0:
+            track['extension'] = 'iso'
+            if self.raw:
+                track['bstart'] = 0
+                track['bsize'] = 2352
+                return track
+            if self.psxtruncate:
+                track['bstart'] = 0
+                track['bsize'] = 2336
+                return track
+            track['bstart'] = 24
+            track['bsize'] = 2048
+            return track
+        if line.lower().find('mode2/2336') >= 0:
+            track['extension'] = 'iso'
+            track['bstart'] = 16
+            track['bsize'] = 2336
+            return track
+        if line.lower().find('audio') >= 0:
+            track['bstart'] = 0
+            track['bsize'] = 2352
+            track['audio'] = True
+            if self.towav:
+                track['extension'] = 'wav'
+            else:
+                track['extension'] = 'cdr'
+            return track
+        raise Exception('Can not handle this mode yet, check bchunk docs')
+
+    def parse_cue(self, cue):
+        print('Cue:', cue)
+        with open(cue, 'r') as c:
+            lines = c.readlines()
+            tracks = []
+
+            for line in lines:
+                # FILE
+                if re.search('^\s*FILE', line):
+                    f = self.get_file_name(line)
+                    s = cue.split('/')
+                    if len(s) > 1:
+                        f = '/'.join(s[:-1]) + '/' + f
+                    if self._file:
+                        if len(tracks) >= 1:
+                            tracks[-1]['stop'] = os.stat(tracks[-1]['bin']).st_size
+                            tracks[-1]['stopsect'] = int(tracks[-1]['stop'] / self.SECTLEN)
+                        
+                    self._file = f
+                    continue
+                # TRACK
+                pos = line.lower().find('track ')
+                if pos >= 0:
+                    track = {'audio': False, 'bsize': -1, 'bstart': -1,
+                             'startsect': -1, 'stopsect': -1,
+                             'bin': self._file}
+                    line = line[pos + 6:]
+                    while line[0] == ' ':
+                        line = line[1:]
+                    pos = line.index(' ')
+                    track['num'] = int(line[:pos])
+                    line = line[pos:]
+                    while line[0] == ' ':
+                        line = line[1:]
+                    track = self.get_mode(track, line)
+                    tracks.append(track)
+                # INDEX
+                pos = line.lower().find('index ')
+                if pos >= 0:
+                    line = line[pos + 6:]
+                    while line[0] == ' ':
+                        line = line[1:]
+                    pos = line.index(' ')
+                    line = line[pos:]
+                    while line[0] == ' ':
+                        line = line[1:]
+                    mins, secs, frames = line.split(':')
+
+                    tracks[-1]['startsect'] = 75 * ( int(mins) * 60 + int(secs) ) + int(frames)
+                    tracks[-1]['start'] = int(tracks[-1]['startsect'] * self.SECTLEN)
+                    if len(tracks) > 1:
+                        if tracks[-2]['stopsect'] < 0:
+                            tracks[-2]['stopsect'] = tracks[-1]['startsect']
+                            tracks[-2]['stop'] = tracks[-1]['start'] - 1
+        if len(tracks) >= 1:
+            tracks[-1]['stop'] = os.stat(tracks[-1]['bin']).st_size
+            tracks[-1]['stopsect'] = int(tracks[-1]['stop'] / self.SECTLEN)
+        self.tracks = tracks
+        
+    def writetrack(self, idx, fn):
+        t = self.tracks[idx]
+        fn = fn + "%02d" % t['num'] + '.' + t['extension']
+        print('Read bin', idx, self.tracks[idx]['bin'])
+        with open(self.tracks[idx]['bin'], "rb") as i:
+            i.seek(t['start'])
+            with open(fn, "wb") as o:
+                print('Write track', fn)
+                reallen = int((t['stopsect'] - t['startsect'] + 1) * t['bsize'])
+
+                if t['audio'] and self.towav:
+                    buf = bytearray(44)
+                    buf[0:4] = bytes('RIFF', encoding='utf-8')
+                    struct.pack_into('<I', buf, 4, reallen + self.WAV_DATA_HLEN + self.WAV_FORMAT_HLEN + 4)
+                    buf[8:12] = bytes('WAVE', encoding='utf-8')
+                    buf[12:16] = bytes('fmt ', encoding='utf-8')
+                    struct.pack_into('<I', buf, 16, 0x10)
+                    struct.pack_into('<H', buf, 20, 0x01)
+                    struct.pack_into('<H', buf, 22, 0x02)
+                    struct.pack_into('<I', buf, 24, 44100)
+                    struct.pack_into('<I', buf, 28, 44100 * 4)
+                    struct.pack_into('<H', buf, 32, 4)
+                    struct.pack_into('<H', buf, 34, 2 * 8)
+                    buf[36:40] = bytes('data', encoding='utf-8')
+                    struct.pack_into('<I', buf, 40, reallen)
+                    
+                    o.write(buf)
+
+                sect = t['startsect']
+                while sect <= t['stopsect']:
+                    buf = i.read(self.SECTLEN)
+                    if len(buf) == 0:
+                        break
+                    if t['audio']:
+                        if self.swapaudio:
+                            for pos in range(t['bstart'], t['bstart'] + t['bsize'], 2):
+                                buf[pos:pos+2] = struct.pack('>H', struct.unpack('<H', buf[pos:pos+2])[0])
+
+                    o.write(buf[t['bstart']:t['bstart'] + t['bsize']])
+                    sect = sect + 1
+        
+    def __init__(self):
+        self._file = None
+        self._raw = False
+        self._psxtruncate = False
+        self._towav = False
+        self._swapaudio = False
+        
+    def open(self, cue):
+        self.parse_cue(cue)
+
+    def bin(self, idx):
+        return self.tracks[idx]['bin']
+
+    @property
+    def raw(self):
+        return self._raw
+
+    @raw.setter
+    def raw(self, value):
+        self._raw = value
+
+    @property
+    def swapaudio(self):
+        return self._swapaudio
+
+    @swapaudio.setter
+    def swapaudio(self, value):
+        self._swapaudio = value
+
+    @property
+    def towav(self):
+        return self._towav
+
+    @towav.setter
+    def towav(self, value):
+        self._towav = value
+
+    @property
+    def psxtruncate(self):
+        return self._psxtruncate
+
+    @psxtruncate.setter
+    def psxtruncate(self, value):
+        self._psxtruncate = value
+        
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', action='store_true',
+                    help='Raw mode for MODE2/2352: write all 2352 bytes from offset 0 (VCD/MPEG)')
+    parser.add_argument('-p', action='store_true',
+                    help='PSX mode for MODE2/2352: write 2336 bytes from offset 24.\n(default MODE2/2352 mode writes 2048 bytes from offset 24)')
+    parser.add_argument('-w', action='store_true',
+                    help='Output audio files in WAV format')
+    parser.add_argument('-s', action='store_true',
+                    help='swapaudio: swap byte order in audio tracks')
+    parser.add_argument('image', nargs=1, help='CUE file for the disk')
+    parser.add_argument('basename', nargs=1, help='Name of output file(s)')
+    args = parser.parse_args()
+
+    bc = bchunk()
+    bc.raw = args.r
+    bc.psxtruncate = args.p
+    bc.towav = args.w
+    bc.swapaudio = args.s
+    bc.open(args.image[0])
+    for i in range(len(bc.tracks)):
+        bc.writetrack(i, args.basename[0])
---
+++ pop-fe.py
@@ -0,0 +1,483 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# A utility to automate building and installing PSX games onto different
+# systems.
+# The current directory where you run this utility from needs to be writable
+# so that we can use it to store temporary files during the conversing process.
+#
+# It supports and can convert games stored in the following types of file
+# formats:
+#  .cue  : CUE file. The preferred option. The actual image file is extracted
+#          from the content of the cue file. If the file-name found inside the
+#          cue is a relative path it is assumed that the bin/img file is stored
+#          in the same directory as the cue file.
+#
+# .bin   : BIN/IMG files. In this case a temporary .cue file will be created
+# .img     in the local directory and used for the conversion. This cue file
+#          will assume that the bin/img file is just one single track of type
+#          MODE2/2352
+#
+# .zip   : ZIP file. The ZIP file will be extracted into the local direcotry
+#          and if a .cue file is found it will be used.
+#
+#
+# The utility supports building and installing the games on various different
+# target platforms:
+#
+# --psp-game-dir <path> : This specifies the path to where a PSP sd card has
+#                         been mounted. The games will be converted into
+#                         an EBOOT.PBP and will be installed as
+#                         <path>/<game-id>/EBOOT.PBP
+#                         The EBOOT.PBP will have a cover icon as well as a
+#                         background image embedded.
+# --retroarch-rom-dir   : The directory where retroarch game images are to be
+#                         installed.
+# --retroarch-thumbnail-dir : Where the coverimage for retroarch should go.
+#
+#
+# Examples:
+# Assume I have connected my PSP with USB and it is shows up as :
+#    /run/media/sahlberg/disk
+#
+# ./pop-fe.py --psp-game-dir=/run/media/sahlberg/disk/PSP/GAME/ /psx/Metal\ Gear\ Solid\ VR\ Missions.cue
+#
+#
+# Multidisk games:
+# If you specify more than one cue file then it is assumed that this is
+# a multidisc game and we will use the first cue file to determine the
+# game id and title to be used for the whole set.
+# 
+#
+#
+# During the conversion process the img/bin file will be temporarily converted
+# into an ISO file as NORMAL01.iso in the current directory.
+# This is in order to open the iso9660 image and read the system.cnf file
+# to extract the game id.
+# You can avoid/skip this step by forcing the game-id from the command line,
+# using (example only):
+#     --game-id=SLUS00957
+#
+# Game art and images are fetched from https://psxdatacenter.com/
+# If a file ICON0.PNG is found in the game directory it assumed to be the
+# cover image and thus we skip pulling it from psxdatacenter.
+# Similarly, if PIC1.PNG is found then it is assumed to be a screenshot
+# or background image and again we skip pulling this file from the site.
+#
+# --fetch-metadata
+# This argument will download and install ICON0.PNG, PIC1.PNG as well as the
+# game id and title in the directory where the game is stored.
+# This requires that the game directory is writeable.
+#
+#
+# https://psxdatacenter.com/
+# https://www.psdevwiki.com/ps3/Eboot.PBP
+# https://www.reddit.com/r/RetroArch/comments/ckqtzd/commandline_programs_to_convert_psx_cd_images_on/
+
+from PIL import Image, ImageDraw, ImageFont
+import argparse
+import io
+import os
+import re
+import random
+import struct
+import sys
+import iso9660      # python-pycdio
+import requests
+import requests_cache
+import datetime
+import zipfile
+from pathlib import Path
+
+from bchunk import bchunk
+from popstation import popstation
+
+PSX_SITE = 'https://psxdatacenter.com/'
+
+def get_gameid_from_iso():
+    iso = iso9660.ISO9660.IFS(source='NORMAL01.iso')
+
+    st = iso.stat('system.cnf', True)
+    if st is None:
+        raise Exception('Could not open system.cnf')
+
+    buf = iso.seek_read(st['LSN'])
+    iso.close()
+
+    idx = buf[1].find('cdrom:')
+    if idx < 0:
+        raise Exception('Could not read system.cnf')
+
+    buf = buf[1][idx:idx+50]
+    idx = buf.find(';1')
+    buf = buf[idx-11:idx]
+    
+    game_id = buf
+    return game_id[:4] + game_id[5:8] + game_id[9:11]
+
+
+def fetch_cached_file(path):
+    ret = requests.get(PSX_SITE + path)
+    print('get', PSX_SITE + path)
+    if ret.status_code != 200:
+        raise Exception('Failed to fetch file ', PSX_SITE + path)
+
+    return ret.content.decode(ret.apparent_encoding)
+
+
+def fetch_cached_binary(path):
+    ret = requests.get(PSX_SITE + path, stream=True)
+    if ret.status_code != 200:
+        raise Exception('Failed to fetch file ', PSX_SITE + path)
+
+    return ret.content
+
+    
+def get_gamelist_from_gamecode(code):
+    if code == 'SLES' or code == 'SCES':
+        return fetch_cached_file('plist.html')
+    if code == 'SLUS' or code == 'SCUS':
+        return fetch_cached_file('ulist.html')
+    if code == 'SLPS' or code == 'SCPS' or code == 'SLPM':
+        return fetch_cached_file('jlist.html')
+
+    raise Exception('Don\'t know the gamlist for %s' % code)
+#'https://psxdatacenter.com/plist.html'
+#	"SCED",
+#	"SLED",
+#	"SLPS",
+#	"SIPS",
+#	"ESPM"
+
+
+def get_game_from_gamelist(game_id, game_list):
+    g = re.findall('games/././' + game_id + '.html', game_list)
+    return fetch_cached_file(g[0])
+
+def get_title_from_game(game_id, game):
+    g = re.findall('<title>.*</title>', game)
+    return g[0][7:-8]
+
+def get_icon0_from_game(game_id, game):
+    g = re.findall('images/covers/././%s.jpg' % game_id, game)
+    return fetch_cached_binary(g[0])
+
+def get_pic1_from_game(game_id, game):
+    # Screenshots might be from a different release of the game
+    # so we can not use game_id
+    filter = 'images/screens/././.*/ss..jpg'
+    return fetch_cached_binary(random.choice(re.findall(filter, game)))
+
+def get_first_bin_in_cue(cue):
+    with open(cue, "r") as f:
+        files = re.findall('".*"', f.read())
+        return files[0][1:-1]
+
+def add_image_text(image, title, idx):
+    # Add a nice title text to the background image
+    # Split it into separate lines
+    #   for ' - '
+    if idx:
+        title = title + ' - (Disc %d of %d)' % idx
+    print('Add image text: title:', title)
+    strings = title.split(' - ')
+    y = 18
+    txt = Image.new("RGBA", image.size, (255,255,255,0))
+    fnt = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSansMono.ttf", 20)
+    d = ImageDraw.Draw(txt)
+    
+    for t in strings:
+        ts = d.textsize(t, font=fnt)
+        d.text((image.size[0] - ts[0], y), t, font=fnt,
+               fill=(255,255,255,255))
+        y = y + ts[1] + 2
+
+    image = Image.alpha_composite(image, txt)
+    return image
+
+def copy_file(inp, oup):
+    with open(inp, "rb") as i:
+        with open(oup, "wb") as o:
+            while True:
+                buf = i.read(1024*1024)
+                if len(buf) == 0:
+                    break
+                o.write(buf)
+
+
+def create_path(bin, f):
+    s = bin.split('/')
+    if len(s) > 1:
+        f = '/'.join(s[:-1]) + '/' + f
+    return f
+
+def get_bin_file(line):
+    # strip off leading 'FILE '
+    pos = line.lower().index('file ')
+    line = line[pos + 5:]
+    # strip off leading 'FILE '
+    pos = line.lower().index(' binary')
+    line = line[:pos+1]
+    #strip off leading ' '
+    while line[0] == ' ':
+        line = line[1:]
+    #strip off trailing ' '
+    while line[-1] == ' ':
+        line = line[:-1]
+    # remove double quotes
+    if line[0] == '"':
+        line = line[1:-1]
+    # remove single quotes
+    if line[0] == '\'':
+        line = line[1:-1]
+    return line
+
+def main(cue, idx, args):
+    with open(cue, 'r') as c:
+        bin = get_bin_file(c.readlines()[0])
+        s = cue.split('/')
+        if len(s) > 1:
+            bin = '/'.join(s[:-1]) + '/' + bin
+
+    print('BIN:', bin)
+
+    game_id = None
+    if args.game_id:
+        game_id = args.game_id
+    if not game_id:
+        try:
+            with open(create_path(bin, 'GAME_ID'), 'r') as d:
+                game_id = d.read()
+        except:
+            True
+    if not game_id:
+        game_id = get_gameid_from_iso()
+    print('Id:', game_id)
+
+    game_list = get_gamelist_from_gamecode(game_id[0:4].upper())
+    game = get_game_from_gamelist(game_id[0:4].upper() + '-' + game_id[4:9], game_list)
+    
+    game_title = None
+    if args.title:
+        game_title = args.title
+    if not game_title:
+        try:
+            with open(create_path(bin, 'GAME_TITLE'), 'r') as d:
+                game_title = d.read()
+        except:
+            True
+    if not game_title:
+        game_title = get_title_from_game(game_id, game)
+    print('Title:', game_title)
+
+
+    if args.fetch_metadata:
+        print('fetching metadata')
+
+        with open(create_path(bin, 'GAME_ID'), 'w') as d:
+            d.write(game_id)
+        with open(create_path(bin, 'GAME_TITLE'), 'w') as d:
+            d.write(game_title)
+
+        try:
+            os.stat(create_path(bin, 'ICON0.PNG'))
+            print('Already have ICON0.PNG')
+        except:
+            print('Fetching cover as ICON0.PNG')
+            icon0 = get_icon0_from_game(game_id[0:4].upper() + '-' + game_id[4:9], game)
+            image = Image.open(io.BytesIO(icon0))
+            image.save(create_path(bin, 'ICON0.PNG'), format='PNG')
+
+        try:
+            os.stat(create_path(bin, 'PIC1.PNG'))
+            print('Already have PIC1.PNG')
+        except:
+            print('Fetching screenshot as PIC1.PNG')
+            pic1 = get_pic1_from_game(game_id[0:4] + '-' + game_id[4:9], game)
+            image = Image.open(io.BytesIO(pic1))
+            image.save(create_path(bin, 'PIC1.PNG'), format='PNG')
+        
+    if args.psp_game_dir:
+        p = popstation()
+        p.img = bin
+        try:
+            os.stat(bin[:-4] + '.ccd')
+            p.ccd = bin[:-4] + '.ccd'
+            print('Found CDD', p.ccd)
+        except:
+            True
+        p.game_id = game_id
+        p.game_title = game_title
+
+        try:
+            image = Image.open(create_path(bin, 'ICON0.PNG'))
+            print('Use existing ICON0.PNG as cover')
+        except:
+            print('Fetch cover for', game_title)
+            icon0 = get_icon0_from_game(game_id[0:4].upper() + '-' + game_id[4:9], game)
+            image = Image.open(io.BytesIO(icon0))
+
+        image = image.resize((80,80), Image.BILINEAR)
+        i = io.BytesIO()
+        image.save(i, format='PNG')
+        i.seek(0)
+        p.icon0 = i.read()
+
+        try:
+            image = Image.open(create_path(bin, 'PIC1.PNG'))
+            print('Use existing PIC1.PNG as background')
+        except:
+            print('Fetch screenshot for', game_title)
+            pic1 = get_pic1_from_game(game_id[0:4] + '-' + game_id[4:9], game)
+            image = Image.open(io.BytesIO(pic1))
+        image = image.resize((480, 272), Image.BILINEAR).convert("RGBA")
+        image = add_image_text(image, game_title, idx)
+
+        i = io.BytesIO()
+        image.save(i, format='PNG')
+        i.seek(0)
+        p.pic1 = i.read()
+
+        f = args.psp_game_dir + '/' + game_id
+        if idx:
+            f = f + ' - %d' % idx[0]
+        print('Install EBOOT in', f)
+        try:
+            os.mkdir(f)
+        except:
+            True
+            
+        print('Running popstation')
+        p.eboot = f + '/EBOOT.PBP'
+        p.convert()
+        
+        return
+    
+    if args.retroarch_thumbnail_dir:
+        g = game_title
+        if idx:
+            g = g + '-%d' % idx[0]
+        icon0 = get_icon0_from_game(game_id[0:4] + '-' + game_id[4:9], game)
+        image = Image.open(io.BytesIO(icon0))
+        try:
+            os.stat(args.retroarch_thumbnail_dir + '/Named_Boxarts')
+        except:
+            os.mkdir(args.retroarch_thumbnail_dir + '/Named_Boxarts')
+        image = image.resize((256,256), Image.BILINEAR)
+        #The following characters in playlist titles must be replaced with _ in the corresponding thumbnail filename: &*/:`<>?\|
+        f = args.retroarch_thumbnail_dir + '/Named_Boxarts/' + g + '.png'
+        print('Save cover as', f)
+        image.save(f, 'PNG')
+
+        pic1 = get_pic1_from_game(game_id[0:4] + '-' + game_id[4:9], game)
+        image = Image.open(io.BytesIO(pic1))
+        try:
+            os.stat(args.retroarch_thumbnail_dir + '/Named_Snaps')
+        except:
+            os.mkdir(args.retroarch_thumbnail_dir + '/Named_Snaps')
+        image = image.resize((512,256), Image.BILINEAR)
+        #The following characters in playlist titles must be replaced with _ in the corresponding thumbnail filename: &*/:`<>?\|
+        f = args.retroarch_thumbnail_dir + '/Named_Snaps/' + g + '.png'
+        print('Save snap as', f)
+        image.save(f, 'PNG')
+
+    if args.retroarch_rom_dir:
+        g = game_title
+        if idx:
+            g = g + '-%d' % idx[0]
+        f = args.retroarch_rom_dir + '/' + g + '.img'
+        print('Installing', f)
+        copy_file(bin, f)
+        
+                     
+# ICON0 is the game cover
+# PIC1 is background image/poster
+#
+if __name__ == "__main__":
+    expire_after = datetime.timedelta(days=100)
+    requests_cache.install_cache(str(Path.home()) + '/.pop-fe', expire_after=expire_after)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--retroarch-thumbnail-dir',
+                    help='Where to store retroarch thumbnails')
+    parser.add_argument('--retroarch-rom-dir',
+                    help='Where to store retroarch roms')
+    parser.add_argument('--psp-game-dir',
+                    help='Where to store PSP EBOOT.PBP')
+    parser.add_argument('--fetch-metadata', action='store_true',
+                    help='Just fetch metadata for the game')
+    parser.add_argument('--game_id',
+                        help='Force game_id for this iso.')
+    parser.add_argument('--title',
+                    help='Force title for this iso')
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args()
+    
+    if not args.files:
+        print('You must specify at least one file to fetch images for')
+        exit(1)
+
+    try:
+        os.unlink('NORMAL01.iso')
+    except:
+        True
+
+    idx = None
+    if len(args.files) > 1:
+        idx = (1, len(args.files))
+    for cue in args.files:
+        zip = None
+        print('Processing', cue, '...')
+
+        if cue[-3:] == 'zip':
+            print('This is a ZIP file. Uncompress the file.')
+            zip = cue
+            with zipfile.ZipFile(zip, 'r') as zf:
+                for f in zf.namelist():
+                    print('Extracting', f)
+                    zf.extract(f)
+                    if re.search('.cue$', f):
+                        print('Found CUE file', f)
+                        cue = f
+
+        tmpcue = None
+        if cue[-3:] == 'img' or cue[-3:] == 'bin':
+            print('IMG or IMG file. Create a temporary cue file for it')
+            tmpcue = 'TMP.cue'
+            with open(tmpcue, "w") as f:
+                f.write('FILE "%s" BINARY\n' % cue)
+                f.write('  TRACK 01 MODE2/2352\n')
+                f.write('    INDEX 01 00:00:00\n')
+
+            cue = tmpcue
+
+        if cue[-3:] != 'cue':
+            print('%s is not a CUE file. Skipping' % cue)
+            continue
+        
+        if not idx or idx[0] == 1:
+            # We need to convert the first track to an ISO so we can open the
+            # disk and read system.cnf
+            # We only do this for the first disk of a multi-disk set.
+            print('Convert CUE to a normal style ISO')
+            bc = bchunk()
+            bc.open(cue)
+            bc.writetrack(0, 'NORMAL')
+
+        main(cue, idx, args)
+        if idx:
+            idx = (idx[0] + 1, idx[1])
+        if tmpcue:
+            os.unlink(tmpcue)
+
+        if zip:
+            with zipfile.ZipFile(zip, 'r') as zf:
+                for f in zf.namelist():
+                    print('Remove temporary file', f)
+                    os.unlink(f)
+
+    try:
+        os.unlink('NORMAL01.iso')
+    except:
+        True
+
