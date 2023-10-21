b35a19dc8867516c20df904d5cb5a7b7b5e4e147
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2023-10-22 07:17:40 +1000

    initial checkin of eboot tool for testing cdda issue
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

---
+++ eboot-tool.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+import argparse
+import io
+import os
+import shutil
+import struct
+import sys
+
+PSP_OFFSET = 0x20
+PSAR_OFFSET = 0x24
+
+
+def getpsisoimgoffset(eboot):
+    with open(eboot, 'rb') as f:
+        # read header
+        buf = f.read(0x28)
+        header = {}
+        header['magic'] = buf[0:4]
+        if header['magic'] != b'\x00PBP':
+            raise Exception('Not a PBP file')
+        header['unknown'] = struct.unpack_from('<I', buf, 4)[0]
+        header['sfo'] = struct.unpack_from('<I', buf, 8)[0]
+        header['icon0'] = struct.unpack_from('<I', buf, 12)[0]
+        header['icon1'] = struct.unpack_from('<I', buf, 16)[0]
+        header['pic0'] = struct.unpack_from('<I', buf, 20)[0]
+        header['pic1'] = struct.unpack_from('<I', buf, 24)[0]
+        header['snd0'] = struct.unpack_from('<I', buf, 28)[0]
+        header['datapsp'] = struct.unpack_from('<I', buf, PSP_OFFSET)[0]
+        header['datapsar'] = struct.unpack_from('<I', buf, PSAR_OFFSET)[0]
+
+        offset = header['datapsar']
+        
+        #print('PSAR starting at 0x%04x' % offset)
+        f.seek(offset)
+        buf = f.read(0x16)
+        if buf[:10] == b'PSTITLEIMG':
+            f.seek(offset + 0x200)
+            buf = f.read(4)
+            offset = header['datapsar'] + struct.unpack_from('<I', buf, 0)[0]
+            #print('PSISOIMG at 0x%04x' % offset)
+                
+        f.seek(offset)
+        buf = f.read(0x16)
+        if buf[:8] == b'PSISOIMG':
+            return offset
+
+def getaudiotracktable(eboot):
+    offset = getpsisoimgoffset(eboot)
+    with open(eboot, 'rb') as f:
+        f.seek(offset + 0xc00)
+        return f.read(0x620)
+
+def printhex(name, buf):
+    print(name)
+    idx = 0
+    for c in buf:
+        if idx % 16 == 0:
+            if idx != 0:
+                print('')
+            print('%04x  ' % idx, end='')
+        print('%02x ' % c, end='')
+        if idx % 16 == 7:
+            print(' ', end='')
+        idx = idx + 1
+    print('')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', action='store_true', help='Verbose')
+    parser.add_argument('--game-id',
+                        help='GameId to inject.')
+    parser.add_argument('--config',
+                        help='Config to inject')
+    parser.add_argument('--eboot', help='Output EBOOT')
+    parser.add_argument('--good-eboot', help='Good EBOOT')
+    parser.add_argument('--bad-eboot', help='Bad EBOOT')
+    args = parser.parse_args()
+
+    if not args.eboot:
+        print('Must specify --eboot')
+        os._exit(1)
+
+
+    print('Copy BAD eboot to test EBOOT')
+    shutil.copyfile(args.bad_eboot, args.eboot)
+    
+    good_offset = getpsisoimgoffset(args.good_eboot)
+    print('GOOD EBOOT PSISOIMG starts at %08x' % good_offset)
+    bad_offset = getpsisoimgoffset(args.bad_eboot)
+    print('BAD  EBOOT PSISOIMG starts at %08x' % bad_offset)
+
+    #good_att = getaudiotracktable(args.good_eboot)
+    #printhex('GOOD ATT', good_att)
+    #bad_att = getaudiotracktable(args.bad_eboot)
+    #printhex('BAD  ATT', bad_att)
+
+    good_toc = getaudiotracktable(args.good_eboot)
+    printhex('GOOD TOC', good_toc)
+    bad_toc = getaudiotracktable(args.bad_eboot)
+    printhex('BAD  TOC', bad_toc)
+    
