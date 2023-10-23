b9838e9c71e88a93216c0e12858707776fbc8e8d
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2023-10-23 17:40:32 +1000

    Add the PAL version of xploder v3
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- gamedb.py
+++ gamedb.py
@@ -22,6 +22,13 @@ games = {
         'pic0': None,
         'pic1': None,
     },
+    'SLES20090': {
+    	'id': 'SLES20090',
+	'title': "XPLODER V3",
+        'icon0': None,
+        'pic0': None,
+        'pic1': None,
+    },
     'PBPX95001': {
     	'id': 'PBPX95001',
 	'title': "DEMO ONE",
