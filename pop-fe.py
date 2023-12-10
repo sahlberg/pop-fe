a2b38a5bf94b22fe4bb4e28198362e139951d20f
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2023-12-10 20:53:59 +1000

    Fix broken pic0 handling triggered by Overblood
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- pop-fe.py
+++ pop-fe.py
@@ -373,30 +373,27 @@ def get_pic_from_game(pic, game_id, game, filename):
 def get_pic0_from_game(game_id, game, cue):
     try:
         pic0 = get_pic_from_game('pic0', game_id, game, cue[:-4] + '_pic0.png')
+        # resize to maximum 1000,560 (ps3 PIC0 size) keeping aspect ratio
+        ar = pic0.height / pic0.width
+        if pic0.height * ar > 560:
+            if int(560 / ar) < 1000:
+                pic0 = pic0.resize((int(560 / ar), 560), Image.Resampling.NEAREST)
+            else:
+                ar = 1000 / pic0.width
+                pic0 = pic0.resize((1000, int(pic0.height * ar)), Image.Resampling.NEAREST)
+            i = Image.new(pic0.mode, (1000, 560), (0,0,0)).convert('RGBA')
+            i.putalpha(0)
+            ns = (int((1000 - pic0.size[0]) / 2), 0)
+            i.paste(pic0, ns)
+            pic0 = i
+        else:
+            pic0 = pic0.resize((1000, int(1000 * ar)), Image.Resampling.NEAREST)
+            i = Image.new(pic0.mode, (1000, 560), (0,0,0)).convert('RGBA')
+            i.putalpha(0)
+            i.paste(pic0, (0, int((560 - pic0.size[1]) / 2)))
+            pic0 = i
     except:
         return None
-    if not pic0:
-        return None
-    # resize to maximum 1000,560 (ps3 PIC0 size) keeping aspect ratio
-    ar = pic0.height / pic0.width
-    if pic0.height * ar > 560:
-        if int(560 / ar) < 1000:
-            pic0 = pic0.resize((int(560 / ar), 560), Image.Resampling.NEAREST)
-        else:
-            ar = 1000 / pic0.width
-            pic0 = pic0.resize((1000, int(pic0.height * ar)), Image.Resampling.NEAREST)
-        i = Image.new(pic0.mode, (1000, 560), (0,0,0)).convert('RGBA')
-        i.putalpha(0)
-        ns = (int((1000 - pic0.size[0]) / 2), 0)
-        i.paste(pic0, ns)
-        pic0 = i
-    else:
-        pic0 = pic0.resize((1000, int(1000 * ar)), Image.Resampling.NEAREST)
-        i = Image.new(pic0.mode, (1000, 560), (0,0,0)).convert('RGBA')
-        i.putalpha(0)
-        i.paste(pic0, (0, int((560 - pic0.size[1]) / 2)))
-        pic0 = i
-
     return pic0
 
 def get_pic1_from_game(game_id, game, cue):
