23009b518502a16d62ab6ebd8b621de478b4486f
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2025-07-07 18:26:16 +1000

    Cant use special importlib on windows pyinstaller build
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- layout.py
+++ layout.py
@@ -2,10 +2,89 @@
 # coding: utf-8
 #
 import argparse
+import hashlib
 import importlib
+import io
+import requests
 from PIL import Image, ImageDraw, ImageFont
-popfe = importlib.import_module("pop-fe")
 from gamedb import games
+
+
+def get_pic_from_game(pic, game_id, game, filename):
+    try:
+        image = Image.open(filename)
+        print('Use existing', filename, 'as', pic) if verbose else None
+        return image
+    except:
+        True
+
+    if game_id in games and pic in games[game_id]:
+        # pic None-ed out in gamedb
+        if not games[game_id][pic]:
+            return None
+
+        _h = hashlib.md5(games[game_id][pic].encode('utf-8')).hexdigest()
+        f = 'https://github.com/sahlberg/pop-fe-assets/raw/master/' + pic + '/' + _h
+        ret = requests.get(f, stream=True)
+        if ret.status_code == 200:
+            print('Found cached prebuilt', pic.upper(), f)
+            return Image.open(io.BytesIO(ret.content))
+    
+        ret = requests.get(games[game_id][pic], stream=True)
+        if ret.status_code == 200:
+            if ret.apparent_encoding:
+                return Image.open(io.BytesIO(ret.content.decode(ret.apparent_encoding)))
+            else:
+                return Image.open(io.BytesIO(ret.content))
+    if not game or game_id[:4] == 'UNKN':
+        return Image.new("RGBA", (80, 80), (255,255,255,0))
+    
+    # Screenshots might be from a different release of the game
+    # so we can not use game_id
+    filter = 'images/screens/./.*/.*/ss..jpg'
+    fcb = fetch_cached_binary(random.choice(re.findall(filter, game)))
+    if not fcb:
+        return None
+    return Image.open(io.BytesIO(fcb))
+
+def get_pic0_from_game(game_id, game, cue):
+    #try:
+    if True:
+        pic0 = get_pic_from_game('pic0', game_id, game, cue[:-4] + '_pic0.png')
+        # If we need to rescale, paste the image into a larger transparent
+        # canvas first before we rescale it below
+        if 'pic0-scaling' in games[game_id]:
+            sc = games[game_id]['pic0-scaling']
+        else:
+            sc = 0.6
+        if 'pic0-offset' in games[game_id]:
+            of = games[game_id]['pic0-offset']
+        else:
+            of = (0.30, 0.30)
+
+        # First, try to scale Y axis to 560 pixels
+        syf = 560 / pic0.size[1]
+        ns = (int(pic0.size[0] * syf), int(pic0.size[1] * syf))
+        if ns[0] > 1000:
+            # Fitting by Y size made it too big. Scale X axis to 1000 instead
+            sxf = 1000 / pic0.size[0]
+            ns = (int(pic0.size[0] * sxf), int(pic0.size[1] * sxf))
+        pic0 = pic0.resize((int(ns[0] * sc), int(ns[1] * sc)), Image.Resampling.LANCZOS)
+        i = Image.new(pic0.mode, (1000, 560), (0,0,0)).convert('RGBA')
+        i.putalpha(0)
+        i.paste(pic0, (int(1000 * of[0]), int(560 * of[1])))
+        pic0 = i
+    #except:
+    #    return None
+
+    return pic0
+
+def get_pic1_from_game(game_id, game, cue):
+    try:
+        return get_pic_from_game('pic1', game_id, game, cue[:-4] + '_pic1.png')
+    except:
+        return None
+
 
 def has_transparency(img):
     if img.info.get("transparency", None) is not None:
@@ -45,9 +124,11 @@ if __name__ == "__main__":
         print('pic0 offset:', games[args.gameid]['pic0-offset'])
     else:
         print('pic0 offset: DEFAULT (0.3, 0.3)')
-        
-    p1 = popfe.get_pic1_from_game(args.gameid, None, 'nothing')
-    p0 = popfe.get_pic0_from_game(args.gameid, None, 'nothing')
+
+    p1 = get_pic1_from_game(args.gameid, None, 'nothing')
+    print('p1', p1)
+    p0 = get_pic0_from_game(args.gameid, None, 'nothing')
+    print('p0', p0)
 
     # PS3
     pic0 = p0.resize((1000,560), Image.Resampling.LANCZOS)
